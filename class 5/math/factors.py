76bed763f9deec236f06b5252c0c240f2cafe8ab
ajaycode <witeajay@gmail.com>
2015-07-04 17:54:44 +0530

    Multiple changes

--- class 5/math/factors.py
+++ class 5/math/factors.py
@@ -2,16 +2,21 @@ __author__ = 'ajay'
 '''factors.py: Generates fractions related mathematical problems'''
 
 import random
+import logging
 
 answers = []
 
 def _is_prime (number):
     if (number < 3):
         return False
-    for i in range (3, number):
-        if (number % i == 0):
+    for i in range (2, number):
+        if number % i == 0:
             return False
     return True
+
+def is_prime (number):
+    '''Written for conducting unit tests only'''
+    return (_is_prime(number))
 
 def nearest_primes ():
     number = random.randint (10, 200)
@@ -51,12 +56,51 @@ def twin_primes ():
     answers.append (answer)
     print ("{} and {} are twin primes.  Yes or No?".format (first_prime, first_prime + 2))
 
-functions = [nearest_primes, list_primes, twin_primes]
+def factors (number=0):
+    if number <= 0:
+        number = random.randint (50, 500)
+    factors = []
+    n = 1
+    while (n <= number):
+        if (_is_prime(n) == True or n == 2):
+         if (number % n == 0):
+             factors.append (n)
+             #factors.append (number / n)
+        n = n + 1
+    answers.append (factors)
+    print ("The factors of {number} are _________________.".format (number=number))
+    return (factors) # for unit testing
 
+def multiples (number=0, number_of_multiples=0):
+    if (number <= 0) or (number_of_multiples <= 0):
+        number = random.randint (16, 200)
+        number_of_multiples = random.randint (4, 8)
+    multiple_list = []
+    for n in range (1, number_of_multiples + 1):
+        multiple_list.append (number * n)
+    answers.append(multiple_list)
+    print ("The first {number_of_multiples} multiples of {number} are ____________".format (number_of_multiples=number_of_multiples, number=number))
+    return (multiple_list)
 
-for i in range (0, len(functions) ):
-    #print (functions [num])
-    f = functions[i]
-    f ()
-for i in range (0, len (answers)):
-    print (i, answers[i])
+functions = [nearest_primes, list_primes, twin_primes, factors, multiples]
+
+def squat ():
+    print ("hello world")
+    for i in range (5):
+        yield (i**2)
+
+def main ():
+    logging.basicConfig (format = '%(asctime)s - %(levelname)s - %(message)s', datefmt= '%Y%m%d %I:%M:%S %p',filename='class.log', level=logging.INFO)
+    for i in range (0, len(functions)):
+        logging.info('Function : %s'%functions [i])
+        f = functions [i]
+        f ()
+    for i in range (0, len (answers)):
+        print (i+1, answers[i])
+    primes = [ _is_prime(x) for x in range (100)]
+    print (primes)
+    for i in squat ():
+        print (i)
+
+if __name__ == "__main__":
+    main ()
