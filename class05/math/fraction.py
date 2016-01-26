f4082cb79253c87c0743eb34c61347185ae4fb04
Ajay <writeajay@gmail.com>
2016-01-26 23:09:32 +0530

    fraction sum,difference, mixed fraction addition, unit tests

--- class05/math/fraction.py
+++ class05/math/fraction.py
@@ -1,50 +1,36 @@
 __author__ = 'Ajay'
 
 '''Generates questions on fractions'''
-'''Usage: python <this_file.py> '''
+'''Usage: python <this_file.py> > test.html''' #test.html can be opened in  a browser to view the fraction problems
 ''' Output: Generates a html file, with the questions and answers'''
+# references : https://docs.python.org/2/library/fractions.html
+#              http://meta.math.stackexchange.com/questions/5020/mathjax-basic-tutorial-and-quick-reference
 
 import random
 import logging
 # from fractions import Fraction
 from fractions import Fraction
 
+
 questions = []
 answers = []
 
+style = r'<style>body{font-size:16;font-family:Verdana;}</style>'
 html_pre_context = r'<html> <head> <title> Quiz on fractions </title>' + r'<script type="text/x-mathjax-config">MathJax.Hub.Config({' + \
   r'tex2jax: {' + r"inlineMath: [ ['$','$'], ['\\(','\\)'] ]" + \
-  r'}' + r'})'+ r'</script><script type="text/javascript" src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" ></script> </head > <body> '
+  r'}' + r'})'+ r'</script><script type="text/javascript" src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" ></script> ' +\
+  style + r'</head > <body> '
 html_post_content = r'</body></html>'
 
+'''Generates a fraction ranging from 1...9/1..9'''
+def __generate_fraction ():
+    return (Fraction(random.randint(1,9),random.randint(1,9) ))
 
-def fractions_sum():
-    fraction1_numerator = random.randint(1, 9)
-    fraction1_denominator = random.randint(1, 9) + fraction1_numerator
-    fraction2_numerator = random.randint(1, 9)
-    fraction2_denominator = random.randint(1, 9) + fraction2_numerator
-    answer = Fraction(fraction1_numerator, fraction1_denominator) + Fraction(fraction2_numerator, fraction2_denominator)
-
-    #print ('{}/{}'.format (fraction2_numerator, fraction2_denominator))
-    question = "The sum of {} and {} is __________".format(printable_fraction(fraction1_numerator, fraction1_denominator),
-                                                                 printable_fraction(fraction2_numerator, fraction2_denominator))
-    answer = printable_fraction (answer.numerator, answer.denominator)
-    answers.append (answer)
-    return (question, answer)
-
-def fractions_difference ():
-    subtrahend_numerator = random.randint(1, 9)
-    subtrahend_denominator = random.randint(1, 11) + subtrahend_numerator
-    difference_numerator = random.randint(1, 5)
-    difference_denominator = random.randint(1, 9) + difference_numerator
-    minuend = Fraction (subtrahend_numerator, subtrahend_denominator) + Fraction (difference_numerator, difference_denominator)
-    question = "The difference between {} and {} is __________".format(printable_fraction(minuend.numerator, minuend.denominator),
-                                                                 printable_fraction(subtrahend_numerator, subtrahend_denominator))
-    answer = minuend - Fraction(subtrahend_numerator, subtrahend_denominator)
-    answer = printable_fraction (answer.numerator, answer.denominator)
-    answers.append (answer)
-    return (question, answer)
-
+def __generate_mixed_fraction ():
+    frac = __generate_fraction()
+    while frac.denominator == 1:
+        frac = __generate_fraction()
+    return frac + random.randint (1,9)
 
 '''input: numerator and denominator
    output: a string, that appears as a fraction in LaTeX format
@@ -53,13 +39,102 @@ def printable_fraction (numerator: int, denominator: int):
     printable = "$\\frac {" + str(numerator) +"}{" +  str(denominator) + "}$"
     return printable
 
-functions = [fractions_sum, fractions_difference]
+'''Takes a Fraction data structure'''
+def printable_fraction_from_fraction (fraction: Fraction):
+    return printable_fraction(fraction.numerator, fraction.denominator)
+
+def printable_mixed_fraction (fraction: Fraction):
+    if fraction.numerator <= fraction.denominator:
+        return printable_fraction_from_fraction(fraction)
+    else:
+        whole_number = fraction.numerator // fraction.denominator
+        fraction = fraction - whole_number
+        printable = "${}".format(whole_number) +"\\frac {" + str(fraction.numerator) +"}{" +  str(fraction.denominator) + "}$"
+        return printable
+
+
+def fractions_sum():
+    fraction1_numerator = random.randint(1, 9)
+    fraction1_denominator = random.randint(1, 9) + fraction1_numerator
+    fraction2_numerator = random.randint(1, 9)
+    fraction2_denominator = random.randint(1, 9) + fraction2_numerator
+    answer = Fraction(fraction1_numerator, fraction1_denominator) + Fraction(fraction2_numerator, fraction2_denominator)
+    question = "The sum of {} and {} is __________".format(printable_fraction(fraction1_numerator, fraction1_denominator),
+                                                                 printable_fraction(fraction2_numerator, fraction2_denominator))
+    answer = printable_fraction (answer.numerator, answer.denominator)
+    return (question, answer)
+
+def fractions_difference ():
+    subtrahend= Fraction (random.randint(1, 9), random.randint(1,9))
+    difference = Fraction (random.randint(1, 9),random.randint(1, 5))
+    minuend = subtrahend + difference
+    question = "The difference between {} and {} is __________".format(printable_fraction_from_fraction(minuend),
+                                                                 printable_fraction_from_fraction(subtrahend))
+    answer = minuend - subtrahend
+    answer = printable_fraction (answer.numerator, answer.denominator)
+    return (question, answer)
+
+
+
+def sort_unlike_fractions (fractions_list = [], ascending=True):
+    list = []
+    if  not fractions_list:
+        i = 0
+        while len (list) < 5:
+            frac = __generate_fraction()
+            if frac not in list:
+                list.append(frac)
+                i+=1
+    else:
+        list = fractions_list
+    logging.info ("Fractions to be sorted: {}".format(list))
+    sorted_fraction_list = sorted(list)
+    if ascending is not True:
+        sorted_fraction_list.reverse()
+    question_sub_text = ""
+    for i in range (0, len(list)):
+        question_sub_text += printable_fraction_from_fraction(list[i])
+        if i != len (list)-1:
+            question_sub_text += ','
+    if ascending is True:
+        order = "ascending"
+    else:
+        order = "descending"
+    question = 'Arrange the fractions {} in {} order'.format (question_sub_text, order )
+    logging.info (question)
+    answer = ""
+    for i in range (0, len(list)):
+        answer += printable_fraction_from_fraction(sorted_fraction_list[i])
+        if i != len (list)-1:
+            answer += ','
+    logging.info (answer)
+    logging.info (sorted_fraction_list)
+
+    return (question, answer)
+
+def sort_unlike_fractions_descending (fractions_list = [], ascending=False):
+    return sort_unlike_fractions(fractions_list, ascending)
+
+def mixed_fractions_sum (number1=Fraction(), number2=Fraction()):
+    if number1 == Fraction() or number2 == Fraction():
+        number1 = __generate_mixed_fraction()
+        number2 = __generate_mixed_fraction()
+    answer = number1 + number2
+    #logging.info (number1, number2, answer)
+    question = "The sum of {} and {} is _____.".format (printable_mixed_fraction(number1), printable_mixed_fraction(number2))
+    return (question, printable_mixed_fraction(answer))
+
+def mixed_fractions_difference ():
+    pass
+
+functions = [fractions_sum, fractions_difference, sort_unlike_fractions, sort_unlike_fractions_descending, mixed_fractions_sum]
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y%m%d %I:%M:%S %p',
                         filename='class.log', level=logging.INFO)
     #random.seed (os.urandom(5))
+
     for i in range(0, len(functions)):
         logging.info('Function : %s' % functions[i])
         f = functions[i]
@@ -68,22 +143,15 @@ def main():
         answers.append(answer)
 
 
-    html_text = r'<ul>'
+    html_text = r'<ol>'
     for i in range(0, len(questions)):
-        html = "<li> {} </li>".format (questions[i])
+        html = "<li> {} : {}</li>".format (questions[i], answers[i])
         html_text += html
 
         #print(answer)
-    #html_text += r'$\frac {2}{9}$'
-    html_text += r'</ul>'
+    #html_text += r'$2\frac {2}{9}$'
+    html_text += r'</ol>'
     print ("{} {} {}".format(html_pre_context, html_text, html_post_content))
-        #primes = [ _is_prime(x) for x in range (100)]
-        #print (primes)
-        #for i in squat ():
-        #    print (i)
-
-        #for i in range (100, 500):
-        #   print (i, all_factors(i))
 
 
 if __name__ == "__main__":
--- class05/math/tests/test_fraction.py
+++ class05/math/tests/test_fraction.py
@@ -1,5 +1,7 @@
-from unittest import TestCase
+# ..\tests>python test_fraction.py
 
+import unittest
+from unittest import TestCase
 __author__ = 'Ajay'
 import sys
 
@@ -8,11 +10,29 @@ from fraction import *
 
 
 class TestFraction(TestCase):
-    def test_fraction(self):
+    def test_printable_fraction(self):
         self.assertEqual(r'$\frac {2}{3}$', printable_fraction(2, 3))
         self.assertEqual(r'$\frac {12}{3}$', printable_fraction(12, 3))
         self.assertEqual(r'$\frac {2}{403}$', printable_fraction(2, 403))
 
+    def test_printable_fraction_from_fraction (self):
+        self.assertEqual(r'$\frac {2}{403}$', printable_fraction_from_fraction(Fraction(2, 403)))
+
+    def test_sort_unlike_fractions(self):
+        question, answer = sort_unlike_fractions ([Fraction(1,2), Fraction (3,4), Fraction (2,3), Fraction (15,16)])
+        self.assertEqual(r'$\frac {1}{2}$,$\frac {2}{3}$,$\frac {3}{4}$,$\frac {15}{16}$', answer)
+        question, answer = sort_unlike_fractions ([Fraction(1,2), Fraction (3,4), Fraction (2,3), Fraction (15,16)], ascending=False)
+        self.assertEqual(r'$\frac {15}{16}$,$\frac {3}{4}$,$\frac {2}{3}$,$\frac {1}{2}$', answer)
+
     def test_fractions_sum (self):
-        #self.assertEqual()
+        self.assertEqual(3, 2+1)
         pass
+
+    def test_mixed_fraction_sum (self):
+        number1 = Fraction(25,3)
+        number2 = Fraction (33,4)
+        question, answer = mixed_fractions_sum(number1, number2)
+        self.assertEqual(r'$16\frac {7}{12}$', answer )
+
+if __name__ == '__main__':
+    unittest.main()
